f218e02f680b92e207fb29b818a4fd7f8705ac74
dizs <dizs@using.ai>
2023-12-13 18:17:00 +0800

    修改为我的主题

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'DZS', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'AI 工程师', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.dzs.life/', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'http://www.dzs.life/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '博客,AI,LLMs,Stable Diffusion,', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
