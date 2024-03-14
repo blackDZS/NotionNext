c2ae08beef2960f3df2717c62cf1cc7b37df1bfd
dizs <dizs@using.ai>
2024-03-14 12:32:27 +0800

    修改为主题

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'DZS', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'AI 工程师', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'http://www.dzs.life/', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '博客,AI,LLMs,Stable Diffusion,', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '博客,AI,LLMs,RAG,Agent', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '1980694906@qq.com', // 邮箱地址 例如mail@tangly1024.com
--- themes/heo/config.js
+++ themes/heo/config.js
@@ -10,25 +10,25 @@ const CONFIG = {
 
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享AI技术',
-  HEO_HERO_TITLE_2: '与思考',
+  HEO_HERO_TITLE_2: '与应用',
   HEO_HERO_TITLE_3: 'DZS.LIFE',
   HEO_HERO_TITLE_4: '',
   HEO_HERO_TITLE_5: '',
   HEO_HERO_TITLE_LINK: '',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '论文阅读', url: '/tag/论文阅读' },
-  HEO_HERO_CATEGORY_2: { title: 'LLMs', url: '/tag/LLMs' },
-  HEO_HERO_CATEGORY_3: { title: 'Stable Diffusion', url: '/tag/SD' },
+  HEO_HERO_CATEGORY_1: { title: 'AI技术栈', url: '/tag/AI技术栈' },
+  HEO_HERO_CATEGORY_2: { title: '模型训练与部署', url: '/tag/模型训练与部署' },
+  HEO_HERO_CATEGORY_3: { title: 'RAG应用', url: '/tag/RAG 应用' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
-  HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
+  HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: true, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
+    '你好！我是'
   ],
   HEO_INFO_CARD_URL: 'https://github.com/blackDZS', // 个人资料底部按钮链接
 
